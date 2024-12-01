3e1d32e45ef2e6f7bcaffec85929ad4dc981067f
4svEXE <4sv.exe@gmail.com>
2024-12-01 13:15:23 +0100

    Auto-generated commit

--- browser/chunk-D4YAIJKH.js
+++
@@ -1,1 +0,0 @@
-import{$a as s,Ga as n,_a as o,jb as i}from"./chunk-3QBWOERT.js";var p=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=n({type:t,selectors:[["app-settings"]],decls:2,vars:0,template:function(e,m){e&1&&(o(0,"p"),i(1,"settings works!"),s())},encapsulation:2})};export{p as SettingsComponent};
