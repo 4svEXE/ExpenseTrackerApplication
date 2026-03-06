075462617a4e8258d1f75de11e5c43d98427ed60
4svEXE <4sv.exe@gmail.com>
2026-03-06 09:55:45 +0100

    фіча кнопки +-

--- src/app/components/layout/nav/nav.component.ts
+++ src/app/components/layout/nav/nav.component.ts
@@ -1,14 +1,29 @@
 import { Component, inject } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-nav',
-  imports: [RouterLink, RouterLinkActive],
+  standalone: true,
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss'
 })
 export class NavComponent {
   private router = inject(Router);
+  isExpanded = false;
+
+  toggleMenu(event: Event) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.isExpanded = !this.isExpanded;
+  }
+
+  navigateTo(type: 'income' | 'expense') {
+    this.isExpanded = false;
+    this.router.navigate(['/new-transaction/categories', type]);
+  }
 
   get transactionLink() {
     const lastType = localStorage.getItem('lastTransactionType') || 'income';
